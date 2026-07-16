0193c2806be04d89b5d86d55da00e3890b685b8a
Celtian <dominik.hladik@seznam.cz>
2026-07-16 21:50:55 +0200

    feat(workflows): coverage added

--- build.zig
+++ build.zig
@@ -43,4 +43,51 @@ pub fn build(b: *std.Build) void {
     const test_step = b.step("test", "Run tests");
     test_step.dependOn(&b.addRunArtifact(mod_tests).step);
     test_step.dependOn(&b.addRunArtifact(cli_tests).step);
+
+    const make_coverage_dirs = b.addSystemCommand(&.{
+        "mkdir",
+        "-p",
+        b.pathFromRoot("coverage/root"),
+        b.pathFromRoot("coverage/cli"),
+        b.pathFromRoot("coverage/merged"),
+    });
+    const mod_coverage_tests = b.addTest(.{
+        .name = "root-coverage",
+        .root_module = mod,
+        .use_llvm = true,
+    });
+    const cli_coverage_tests = b.addTest(.{
+        .name = "cli-coverage",
+        .root_module = cli_mod,
+        .use_llvm = true,
+    });
+
+    const mod_coverage = b.addSystemCommand(&.{
+        "kcov",
+        b.fmt("--include-path={s}", .{b.pathFromRoot("src")}),
+        b.pathFromRoot("coverage/root"),
+    });
+    mod_coverage.addArtifactArg(mod_coverage_tests);
+    mod_coverage.step.dependOn(&make_coverage_dirs.step);
+
+    const cli_coverage = b.addSystemCommand(&.{
+        "kcov",
+        b.fmt("--include-path={s}", .{b.pathFromRoot("src")}),
+        b.pathFromRoot("coverage/cli"),
+    });
+    cli_coverage.addArtifactArg(cli_coverage_tests);
+    cli_coverage.step.dependOn(&make_coverage_dirs.step);
+
+    const merge_coverage = b.addSystemCommand(&.{
+        "kcov",
+        "--merge",
+        b.pathFromRoot("coverage/merged"),
+        b.pathFromRoot("coverage/root"),
+        b.pathFromRoot("coverage/cli"),
+    });
+    merge_coverage.step.dependOn(&mod_coverage.step);
+    merge_coverage.step.dependOn(&cli_coverage.step);
+
+    const coverage_step = b.step("coverage", "Run tests and generate code coverage");
+    coverage_step.dependOn(&merge_coverage.step);
 }
